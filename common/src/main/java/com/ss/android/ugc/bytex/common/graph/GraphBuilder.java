a3b9245e101574f06384f18a6a99034a2ed4bd41
sarry <758481447@qq.com>
2025-03-17 13:57:46 +0800

    feat:bouncycastle

--- common/src/main/java/com/ss/android/ugc/bytex/common/graph/GraphBuilder.java
+++ common/src/main/java/com/ss/android/ugc/bytex/common/graph/GraphBuilder.java
@@ -50,7 +50,7 @@ public class GraphBuilder {
                         && !"module-info".equals(current.entity.name)
                         && !"retrofit2/DefaultMethodSupport".equals(current.entity.name)
                         && !current.entity.name.startsWith("kotlin/reflect/")
-                        && !current.entity.name.startsWith("org/bouncycastle/jcajce/")
+                        && !current.entity.name.startsWith("org/bouncycastle/")
                 ) {
                     throw new DuplicateClassException(msg);
                 } else {
